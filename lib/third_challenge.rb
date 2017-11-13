ce00af879d57073f05416c895b86f5962dc6b3ba
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:12:33 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         :name => "Romeo",
-        :age => 15,
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
