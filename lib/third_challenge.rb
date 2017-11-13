0ba9d081ceb8f053ac0693a6d5a4b85cd61faf75
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:10:31 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,6 +13,9 @@ def third_challenge
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => 15,
+        :status => "alive"
         },
       :hero_friends => []
    },
