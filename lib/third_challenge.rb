98a3de12c9138f74467500cbb95e80dc6058992a
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:11:26 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,9 +22,13 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
