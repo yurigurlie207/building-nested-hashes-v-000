593df43440a413a7cf6afcf8fd8d9f055d9d52fd
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:08:53 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        
+        :name => "Lord Montague"
         },
       :matriarch => {
         #your key/value pairs here
@@ -12,7 +12,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -27,5 +27,5 @@ def third_challenge
    }
   }
 
-  
+
 end
