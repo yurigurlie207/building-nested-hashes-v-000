a40fb125b461600ce53989c74357341303dbf980
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:15:46 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {name: "Steven", age: "30", attitude: "confused"},{}
+        {name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
