df79e5e88d9dfa20fde65da8058268406394de55
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:14:36 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,10 +4,11 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},{name: "Mercutio", age: "18", attitude: "hot-headed"}
+      :hero_friends => [
         #your hashes here!
+        {name: "Benvolio", age: "17", attitude: "worried"},{name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +19,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
